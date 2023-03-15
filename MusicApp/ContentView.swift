7d01860582269f3c9cab26c6f64d7da831396c27
rsugaya <rsugaya@llc-noa.net>
2023-03-15 19:15:48 +0900

    Initial Commit

---
+++ MusicApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MusicApp
+//
+//  Created by 菅谷亮太 on 2023/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by 菅谷亮太 on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
